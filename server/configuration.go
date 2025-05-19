0d1a957fc5ef2dbeafc609f92d4ee8311e9c4316
Alireza Ghaffari <arg@pardisco.co>
2025-05-19 13:40:53 +0330

    fix

--- server/configuration.go
+++ server/configuration.go
@@ -18,6 +18,9 @@ import (
 // If you add non-reference types to your configuration struct, be sure to rewrite Clone as a deep
 // copy appropriate for your types.
 type Configuration struct {
+	MuChatApiKey string // API key for authenticating with the MuChat service
+	AgentID      string // Agent ID for sending messages to MuChat
+	EnableDebug  bool   // Enable debug mode for additional logging
 }
 
 // Clone shallow copies the configuration. Your implementation may require a deep copy if
